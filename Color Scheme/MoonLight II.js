8a13dc64e60c41fb17f7e101ec3d932983b610d4
Buntu0001 <grradis@grradis.com>
2022-01-15 15:18:34 +0900

    Add "MoonLight II"

---
+++ Color Scheme/MoonLight II.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#191A2A", "#FF757F", "#C3E88D", "#FFC777", "#82AAFF", "#C099FF", "#86E1FC", "#C8D3F5", "#828BB8", "#FF757F", "#C3E88D", "#FFC777", "#82AAFF", "#C099FF", "#86E1FC", "#C8D3F5"]);
+t.prefs_.set('foreground-color', "#C8D3F5");
+t.prefs_.set('background-color', "#222436");
+t.prefs_.set('cursor-color', 'rgba(255,255,255,0.5)');
